131029efe2209f22e35489fe351e6bce61335830
Antonio <antonio.gongora1491@gmail.com>
2022-05-06 17:48:38 -0500

    iniciando proyecto cursos l8

---
+++ database/migrations/2022_05_06_213251_create_curses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curses', function (Blueprint $table) {
+            $table->id();
+            //lo asignamos a un usuario
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image');
+            $table->text('description');
+            $table->timestamps();
+
+            //creamos la relación que necesitemos
+            $table->foreign('user_id')->references('id')->on('users');
+            //categoría está trabajando con la tabla de categorias
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curses');
+    }
+}
---
+++ database/migrations/2022_05_06_213632_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            //un post va a perntenecer a un curso
+            $table->unsignedInteger('course_id');
+            $table->string('name');
+            //se le asigna por default que no son gratis
+            $table->boolean('free')->default(0);
+
+            $table->timestamps();
+
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
